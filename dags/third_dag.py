4e058f5a50173a07588f2f3fabf947799af76a83
MarcusMix <tradedomarcus@gmail.com>
2024-06-04 22:26:07 -0300

    feat: third dag

---
+++ dags/third_dag.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+my_dag = DAG(
+    "third_dag", 
+    description="My third dag",
+    schedule_interval=None, 
+    start_date=datetime(2024, 6, 4),
+    catchup=False
+    )
+
+task1 = BashOperator(task_id="tsk1", bash_command="sleep 5", dag=my_dag)
+task2 = BashOperator(task_id="tsk2", bash_command="sleep 5", dag=my_dag)
+task3 = BashOperator(task_id="tsk3", bash_command="sleep 5", dag=my_dag)
+
+[task1, task2] >> task3  
